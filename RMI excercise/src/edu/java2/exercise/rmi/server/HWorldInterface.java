e0befe26cb3444e23bf61fd1967a3852b98821ae
apgrnt <apgrnt@172.19.166.197>
2015-01-09 12:14:29 -0800

    Initial upload

--- RMI excercise/src/edu/java2/exercise/rmi/server/HWorldInterface.java
+++
@@ -1,8 +0,0 @@
-package edu.java2.exercise.rmi.server;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface HWorldInterface extends Remote {
-    String getComment() throws RemoteException;
-}
